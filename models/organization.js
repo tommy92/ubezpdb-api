99b890cd9cf20895a7a74bf595dff8d19b208fa4
Tomasz Jaworski <tomek.jaworski9@wp.pl>
2015-01-15 17:29:06 +0100

    Add configuration to organizations model to automatically update 'created_at' and 'updated_at' fields within the database

--- models/organization.js
+++ models/organization.js
@@ -1,7 +1,8 @@
 var db = require('../database');
 
 var Organization = db.Model.extend({
-  tableName: 'organizations'
+  tableName: 'organizations',
+  hasTimestamps: ['created_at', 'updated_at']
 });
 
 module.exports = Organization;
